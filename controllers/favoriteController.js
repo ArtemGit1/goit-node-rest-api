01dea5072a496932af9d2343cc2e7d08b2347af2
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2024-02-07 16:37:15 +0200

    123

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -4,10 +4,15 @@ const HttpError = require('../helpers/HttpError');
 const updateFavoriteStatus = async (req, res) => {
   const { contactId } = req.params;
   const { favorite } = req.body;
+  const token = req.headers.authorization;
 
   try {
-    if (favorite === undefined) {
+    if (!token) {
+      res.status(401).json({ message: 'Unauthorized' });
+      return;
+    }
 
+    if (favorite === undefined) {
       res.status(400).json({ message: 'Body must have a "favorite" field' });
       return;
     }
@@ -17,11 +22,9 @@ const updateFavoriteStatus = async (req, res) => {
     if (updatedContact) {
       res.status(200).json(updatedContact);
     } else {
-
       res.status(404).json({ message: 'Not found' });
     }
   } catch (error) {
-
     res.status(error.statusCode || 500).json({ message: error.message });
   }
 };
